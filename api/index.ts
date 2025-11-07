4ded13d7f0b6d0abb1326c79b76ca199dd3d2520
MohamedEmam86 <143082041+MohamedEmam86@users.noreply.github.com>
2025-11-08 01:16:33 +0200

    Remove dotenv configuration from index.ts
    Removed dotenv import and configuration.

--- api/index.ts
+++ api/index.ts
@@ -4,9 +4,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
